470df2cf6d2d0a45d657fa7954d7a767f3eb4c38
joabresper <joabresper@gmail.com>
2022-07-15 15:53:49 -0300

    14/07/2022

--- Modelos.rb
+++ Modelos.rb
@@ -109,9 +109,193 @@ ACCION Modelo_1 ES
 		CERRAR(arch); CERRAR(arch_sal)	
 FIN_ACCION
 
+=================================================================================================================================
+
 ACCION Modelo_2 ES
 	AMBIENTE
-		
+		sec: Secuencia de caracter
+		v: caracter
+
+		cant_letras: N(3)
+		cant_por_lab: entero
+		cant_total: entero
+		id_caracter: AN(1)
+		linea: ("A", "G", "R")
+
+		FUNCION Nro_1al9_a_caracter(nro: N(2)): AN(1) ES
+			AMBIENTE
+				id: N(1)
+			PROCESO
+				Si nro<10 entonces
+					id := nro
+				Sino
+					id := (nro DIV 10) + (nro MOD 10)
+				FinSi
+
+				Segun id hacer
+					1:	Nro_en_caracter := "1"
+					2:	Nro_en_caracter := "2"
+					3:	Nro_en_caracter := "3"
+					4:	Nro_en_caracter := "4"
+					5:	Nro_en_caracter := "5"
+					6:	Nro_en_caracter := "6"
+					7:	Nro_en_caracter := "7"
+					8:	Nro_en_caracter := "8"
+					9:	Nro_en_caracter := "9"
+				FinSegun
+		FIN_FUNCION
+
 	PROCESO
-		
+		ARRANCAR(sec); AVANZAR(sec, v)
+
+		ESCRIBIR("Indique la letra correspondiente a la linea terapeutica de los productos que desea consultar: ")
+		ESCRIBIR("(A) Antiinflamatorio")
+		ESCRIBIR("(G) Gastroenterologica")
+		ESCRIBIR("(R) Respitatoria/Antialergica")
+		LEER(linea)
+
+		Mientras NFDS(sec) hacer
+			# Escribir y Avanzar nombre laboratorio
+			ESCRIBIR("Laboratorio: ")
+			Mientras NFDS(sec) y v<>"-" hacer
+				ESCRIBIR(v)
+				AVANZAR(sec, v)
+			FinMientras
+			# Resetear contador de productos por laboratorio
+			cant_por_lab := 0
+			# Avanzar guion
+			AVANZAR(sec, v)
+			# Tratar productos
+			Mientras NFDS(sec) y v<>"@" hacer
+				# Escribir productos de la linea. Caso contrario, avanzarlos
+				Si v=linea entonces
+					# Contar producto
+					cant_por_lab := cant_por_lab + 1
+					# Avanzar caracter indicador de la linea elegida
+					AVANZAR(sec, v)
+					# Resetear contador de letras
+					cant_letras := 0
+					# Escribir nombre del producto y actualizar contador
+					Mientras v<>"@" y v<>" " hacer
+						cant_letras := cant_letras + 1
+						ESCRIBIR(sec, v)
+						AVANZAR(sec, v)
+					FinMientras
+					# Escribir caracter verificador
+					id_caracter := Nro_1al9_a_caracter(cant_letras)
+					ESCRIBIR(sec, id_caracter)
+					# Escribir espacio en blanco
+					ESCRIBIR(sec, " ")
+				Sino
+					Mientras v<>"@" y v<>" " hacer
+						AVANZAR(sec, v)
+					FinMientras
+				FinSi
+
+				# Avanzar blancos finales
+				Mientras v<>"@" y v=" " hacer
+					AVANZAR(sec, v)
+				FinMientras
+			FinMientras
+
+			# Informar cantidad de productos de la linea por laboratorio
+			ESCRIBIR("Cantidad de productos de la linea '", linea, "': ", cant_por_lab)
+			# Acumular total
+			cant_total := cant_total + cant_por_lab
+
+			# Avanzar simbolo arroba
+			AVANZAR(sec, v)
+		FinMientras
+		# Informar cantidad total de productos de la linea
+		ESCRIBIR("Total General de productos de la linea '", linea, "': ", cant_total)
+
+		CERRAR(sec)
 FIN_ACCION
+
+=================================================================================================================================
+
+ACCION Modelo_3 ES
+	AMBIENTE
+		sec: Secuencia de caracter
+		v: caracter
+		sal: Secuencia de caracter
+
+		numeros = ("1", "2", "3", "4", "5", "6")
+		correcion: caracter
+		error: logico
+		orac_con_error: entero
+		orac_sin_error: entero
+		pal_con_error: entero
+		pal_total: entero
+
+		FUNCION Corregir_caracteres(car: AN(1)): AN(1) ES
+			Segun car hacer
+				"1":	Corregir_caracteres := "q"
+				"2":	Corregir_caracteres := "w"
+				"3":	Corregir_caracteres := "e"
+				"4":	Corregir_caracteres := "r"
+				"5":	Corregir_caracteres := "t"
+				"6":	Corregir_caracteres := "y"
+			FinSegun
+		FIN_FUNCION
+
+	PROCESO
+		ARRANCAR(sec); AVANZAR(sec, v)
+		CREAR(sal)
+
+		pal_con_error, orac_con_error, orac_sin_error, pal_total := 0
+		
+		Mientras NFDS(sec) hacer
+			error := Falso
+			# Tratar oraciones
+			Mientras NFDS(sec) y v<>"." hacer
+				# Tratar palabra impar
+				Mientras v<>"." y v<>" " hacer
+					Si (v en numeros) entonces
+						correcion := Corregir_caracteres(v)
+						ESCRIBIR(sal, correcion)
+						error := Verdadero
+						pal_con_error := pal_con_error + 1
+					Sino
+						ESCRIBIR(sal, v)
+					FinSi
+					AVANZAR(sec, v)
+				FinMientras
+				# Contar palabra impar
+				pal_total := pal_total + 1
+				# Avanzar espacio en blanco si no es final de oracion
+				Si v=" " entonces
+					ESCRIBIR(sal, v)
+					AVANZAR(sec, v)
+				FinSi
+				# Avanzar palabra par
+				Mientras v<>"." y v<>" " hacer
+					ESCRIBIR(sal, v)
+					AVANZAR(sec, v)
+				FinMientras
+				# Avanzar espacio en blanco si no es final de oracion
+				Si v=" " entonces
+					ESCRIBIR(sal, v)
+					AVANZAR(sec, v)
+				FinSi
+				# Contar palabra par
+				pal_total := pal_total + 1
+			FinMientras
+			# Avanzar punto
+			ESCRIBIR(sal, v)
+			AVANZAR(sec, v)
+			# Evaluar si la oracion tuvo correcciones
+			Si error entonces
+				orac_con_error := orac_con_error + 1
+			Sino
+				orac_sin_error := orac_sin_error + 1
+		FinMientras
+
+		ESCRIBIR("Oraciones con alguna palabra modificada: ", orac_con_error)
+		ESCRIBIR("Oraciones sin palabras modificadas: ", orac_sin_error)
+		ESCRIBIR("Total de palabras modificadas en todo el texto: ", pal_con_error)
+		ESCRIBIR("Porcentaje de palabras modificadas sobre el total del texto: ", (pal_con_error/pal_total)*100, "%")
+
+		CERRAR(sec); CERRAR(sal)c
+FIN_ACCION
+
